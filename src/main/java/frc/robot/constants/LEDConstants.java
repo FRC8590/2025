fc0f85d825308a78b28805f10e76877b734d1724
Connor Geraghty <connoriyg@gmail.com>
2025-03-02 11:03:24 -0500

    Created LED subsystem with red, yellow, green, and rainbow

---
+++ src/main/java/frc/robot/constants/LEDConstants.java
@@ -0,0 +1,11 @@
+package frc.robot.constants;
+
+public record LEDConstants(
+    int LEDPort,
+    int length
+) {
+    public static final LEDConstants DEFAULT = new LEDConstants(
+        0,    // LED Port
+        60
+    );
+} 
---
+++ src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -0,0 +1,91 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import static edu.wpi.first.units.Units.Meters;
+import static edu.wpi.first.units.Units.MetersPerSecond;
+
+import org.ironmaple.simulation.drivesims.COTS;
+
+import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.LEDPattern;
+import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.units.Units.*;
+import frc.robot.Constants;
+
+public class LEDSubsystem extends SubsystemBase {
+  /** Creates a new LEDSubsystem. */
+  
+  //LED Objects
+  private AddressableLED m_LED = new AddressableLED(Constants.LED_CONSTANTS.LEDPort());
+  private AddressableLEDBuffer m_LEDBuffer = new AddressableLEDBuffer(Constants.LED_CONSTANTS.length());
+  private LEDPattern desiredPattern = LEDPattern.solid(Color.kRed);
+
+  //LED Configurations
+  private final Distance kLedSpacing = Meters.of(1 / 120.0);
+
+
+  //LED States
+  private final LEDPattern fullRed = LEDPattern.solid(Color.kRed);
+  private final LEDPattern fullGreen = LEDPattern.solid(Color.kGreen);
+  private final LEDPattern fullYellow = LEDPattern.solid(Color.kYellow);
+  private final LEDPattern rainbow = LEDPattern.rainbow(255, 128);
+  private final LEDPattern scrollingRainbow = rainbow.scrollAtAbsoluteSpeed(MetersPerSecond.of(1), kLedSpacing);
+
+  public LEDSubsystem(){
+    m_LED.setLength(m_LEDBuffer.getLength());
+    m_LED.setData(m_LEDBuffer);
+    m_LED.start();
+  
+  }
+
+  public void setRed(){
+    desiredPattern = fullRed;
+  }
+  public void setGreen(){
+    desiredPattern = fullGreen;
+  }
+  public void setYellow(){
+    desiredPattern = fullYellow;
+  }
+  public void setRainbow(){
+    desiredPattern = scrollingRainbow;
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    
+    if(desiredPattern.equals(fullYellow)){
+
+      fullYellow.applyTo(m_LEDBuffer);
+      m_LED.setData(m_LEDBuffer);
+
+    }
+    else if(desiredPattern.equals(scrollingRainbow)){
+
+      scrollingRainbow.applyTo(m_LEDBuffer);
+      m_LED.setData(m_LEDBuffer);
+
+    }
+    else if(desiredPattern.equals(fullRed)){
+
+      fullRed.applyTo(m_LEDBuffer);
+      m_LED.setData(m_LEDBuffer);
+
+    }
+    else if(desiredPattern.equals(fullGreen)){
+
+      fullGreen.applyTo(m_LEDBuffer);
+      m_LED.setData(m_LEDBuffer);
+
+    }
+
+  }
+}
