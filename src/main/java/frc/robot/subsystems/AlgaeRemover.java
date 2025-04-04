2ca464052d959dfb6a3baec345858d8d2dd190e3
Connor Geraghty <connoriyg@gmail.com>
2025-04-04 13:24:09 -0400

    new algae remover deploy position

--- src/main/java/frc/robot/subsystems/AlgaeRemover.java
+++ src/main/java/frc/robot/subsystems/AlgaeRemover.java
@@ -112,7 +112,7 @@ public class AlgaeRemover extends SubsystemBase {
   }
 
   public void reachGoalDown() {
-    pivotMotor.set(pidController.calculate(getPivotPosition(), 0.2));   
+    pivotMotor.set(pidController.calculate(getPivotPosition(), 0.135));   
     runRemover();
  
     
